08e7a86f123d284300005fb94f06db742c52957a
王艺霖 <yilin-wa16@mails.tsinghua.edu.cn>
2019-04-12 04:20:34 +0800

    1.26

--- egnima/1.26.py
+++ egnima/1.26.py
@@ -1,19 +1,25 @@
-text="MYAMRARUYIQTENCTORAHROYWDSOYEOUARRGDERNOGW!!!!!!"
-ans=""
-m,n=4,3
-for i in range(0,4):
-	for j in range(0,n):
-		for k in range(0,m):
-			if text[i*m*n+j+k*n]!="!":
-				print(text[i*m*n+j+k*n])
-				ans=ans+text[i*m*n+j+k*n]
-print(ans)
+import itertools
+text="MYAMRARUYIQTENCTORAHROYWDSOYEOUARRGDERNOGW"
 '''m=[2,3,6,7,14,21]
 n=[21,14,7,6,3,2]
+def getList(x):
+	com_num=[]
+	for i in range(0,x):
+		com_num.append(i)
+	return (list(itertools.permutations(com_num, x)))
 for i in range(0,6):
-	ans=""
-	for j in range(0,n[i]):
-		for k in range(0,m[i]):
-			ans=ans+text[j+k*n[i]]
-	print(ans)'''
+	for k in getList(m[i]):
+		ans=""
+		for j in range(0,n[i]):
+			for t in k:
+				ans=ans+text[t+j*m[i]]
+		print("m="+str(m[i])+" , "+"n="+str(n[i])+" : ")
+		print(ans)'''
+	
+# m=6,n=7,key=(1,3,5,2,4,6)
+ans=""
+for i in range(0,7):
+	for t in (0,2,4,1,3,5):
+		ans=ans+text[t+i*6]
+print (ans)
 	
